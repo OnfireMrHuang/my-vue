dd2ef8a14d2760f0514248cb3ead15fb9ec05524
OnfireMrHuang <engineer_mrhuang@163.com>
2020-04-20 13:17:57 +0800

    单文件组件学习提交

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ message }}
+    <div :id="message">
+      <todo-list>
+        <todo-item v-for='item in list' :key='item.title' :title='item.title' :del='item.del'>
+          <template v-slot:pre-icon='{value}'>
+            <span>前置图标 {{value}}</span>
+          </template>
+        </todo-item>
+      </todo-list>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoItem,
+    TodoList,
+  },
+  data() {
+    return {
+      message: 'Hello Vue!',
+      list:[
+        {
+          title:"课程1",
+          del:false,
+        },
+        {
+          title:"课程2",
+          del:false,
+        }
+      ],
+    }
+  },
+  methods:{
+    handleDelete(val){
+      console.log("删除课程:"+val)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-demo/src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <slot></slot>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods:{
+        handleDelete(val){
+            console.log("删除课程:"+val)
+        }
+    }
+}
+</script>
