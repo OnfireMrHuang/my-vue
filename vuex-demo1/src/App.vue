7981b320219abcfb339194820e2f0922d5e63353
OnfireMrHuang <engineer_mrhuang@163.com>
2020-04-29 16:25:55 +0800

    提交vuex demo

--- vuex-demo1/src/App.vue
+++ vuex-demo1/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>it's my test</h2>
+    {{count}}
+    <br>
+    {{$store.getters.doubleCount}}
+    <br>
+    <button @click="$store.commit('increment',2)">count++</button>
+    <button @click="$store.dispatch('increment')">count++</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed:{
+    count() {
+      return this.$store.state.count
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
