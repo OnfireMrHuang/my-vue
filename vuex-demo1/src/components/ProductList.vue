afd7a6e3d8c4f8793a5e3b9d24dd2d03d1845770
OnfireMrhuang <engineer_mrhuang@163.com>
2020-05-06 21:54:29 +0800

    设置数量可选

--- vuex-demo1/src/components/ProductList.vue
+++ vuex-demo1/src/components/ProductList.vue
@@ -3,6 +3,12 @@
     <li v-for="product in products" :key="product.id">
       {{product.title}} - {{product.price}}
       <br>
+      <div>
+        数量：
+        <select v-model="numbers[product.id]">
+          <option v-for="n in product.inventory" :key="n" :value="n">{{n}}</option>
+        </select>
+      </div>
       <button :disabled="!product.inventory" @click="addProductToCart(product)">
         加入购物车
       </button>
@@ -12,15 +18,41 @@
 
 <script>
 
-import {mapState,mapActions} from "vuex"
+import {mapState} from "vuex"
 
 export default {
+
+  data() {
+    return {
+      numbers:{}
+    }
+  },
+  watch: {
+    products: {
+      handler: function(val) {
+        val.forEach(product=>{
+          if(this.numbers[product.id] === undefined) {
+            this.$set(this.numbers,product.id,1)
+          }
+        })
+      }
+    }
+  },
   computed:mapState({
     products: state =>state.products.all
   }),
-  methods:mapActions('cart',[
-    'addProductToCart'
-  ]),
+  // methods:mapActions('cart',[
+  //   'addProductToCart'
+  // ]),
+  methods: {
+    addProductToCart(product) {
+      this.$store.dispatch(
+        "cart/addProductToCart",
+        {product,number:this.numbers[product.id]}
+      )
+      this.numbers[product.id] =1
+    }
+  },
   created() {
     this.$store.dispatch('products/getAllProducts')
   }
