f49da3499dd4e49dddf679831dab354a461832ff
huangwenwu <xiaowu@mail.btc38.com>
2020-05-07 21:47:38 +0800

    设置webpack配置和babel配置

--- my-admin/babel.config.js
+++ my-admin/babel.config.js
@@ -1,3 +1,7 @@
+const plugins = [
+  ['import', {'libraryName': 'ant-design-vue','libraryDirectory': 'es','style': true}]
+];
+
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+  plugins
+}
