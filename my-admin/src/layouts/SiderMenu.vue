2ba06806e8946b1440da82db9617291293ec9ecc
huangwenwu <xiaowu@mail.btc38.com>
2020-05-25 00:06:34 +0800

    提交代码

--- my-admin/src/layouts/SiderMenu.vue
+++ my-admin/src/layouts/SiderMenu.vue
@@ -19,38 +19,8 @@
 </template>
 
 <script>
-import { Menu } from "ant-design-vue";
-const SubMenu = {
-  template: `
-      <a-sub-menu :key="menuInfo.key" v-bind="$props" v-on="$listeners">
-        <span slot="title">
-          <a-icon type="mail" /><span>{{ menuInfo.title }}</span>
-        </span>
-        <template v-for="item in menuInfo.children">
-          <a-menu-item v-if="!item.children" :key="item.key">
-            <a-icon type="pie-chart" />
-            <span>{{ item.title }}</span>
-          </a-menu-item>
-          <sub-menu v-else :key="item.key" :menu-info="item" />
-        </template>
-      </a-sub-menu>
-    `,
-  name: "SubMenu",
-  // must add isSubMenu: true
-  isSubMenu: true,
-  props: {
-    theme: {
-      type: String,
-      default: "dark"
-    },
-    ...Menu.SubMenu.props,
-    // Cannot overlap with properties within Menu.SubMenu.props
-    menuInfo: {
-      type: Object,
-      default: () => ({})
-    }
-  }
-};
+import SubMenu from "./SubMenu";
+
 export default {
   components: {
     "sub-menu": SubMenu
